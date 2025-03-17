100cd614513213b7916797a78dd69c29e5cc3d15
PikaThorachu <krullwarriorking@gmail.com>
2025-03-17 14:31:08 -0400

    Imported sys into main(), created exit w/ error code

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from stats import get_book_text, get_num_words, get_num_letters, chars_dict_to_sorted_list
+import sys
 
 def main():
-    path = "books/frankenstein.txt"
-    text = get_book_text(path)
+    if len(sys.argv) <2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+    text = get_book_text(sys.argv[1])
     num_words = get_num_words(text)
     chars_dict = get_num_letters(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
